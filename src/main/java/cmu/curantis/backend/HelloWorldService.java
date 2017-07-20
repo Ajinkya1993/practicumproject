e9439d10b5cc2c3c3339e61388392d1977a33289
Namita-S <namitas@andrew.cmu.edu>
2017-07-20 14:28:01 -0400

    Hibernate has been set up

--- src/main/java/cmu/curantis/backend/HelloWorldService.java
+++ src/main/java/cmu/curantis/backend/HelloWorldService.java
@@ -4,18 +4,65 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Produces;
+
  
-@Path("/hello")
-public class HelloWorldService {
+import cmu.curantis.backend.Employee;
+import cmu.curantis.backend.EmployeeDAO;
  
-	@GET
-	@Path("/{param}")
-	public Response getMsg(@PathParam("param") String msg) {
- 
-		String output = "Hi " + msg + ". Welcome to Curantis Solutions Backend Service";
- 
-		return Response.status(200).entity(output).build();
- 
+	
+	@Path("/employees")
+	public class HelloWorldService {
+	 
+	    @GET
+	    @Produces("application/text")
+	    public String getEmployee() {
+	        EmployeeDAO dao = new EmployeeDAO();
+	        List employees = dao.getEmployees();
+	        System.out.println(employees.get(0).toString());
+	        return "";
+	    }
+	 
+	    
+	    @POST
+	    @Path("/create")
+	    @Consumes("application/json")
+	    public Response addEmployee(Employee emp){
+	        emp.setName(emp.getName());
+	        emp.setAge(emp.getAge());
+	                
+	        EmployeeDAO dao = new EmployeeDAO();
+	        dao.addEmployee(emp);
+	        
+	        return Response.ok().build();
+	    }
+	    
+	    @PUT
+	    @Path("/update/{id}")
+	    @Consumes("application/json")
+	    public Response updateEmployee(@PathParam("id") int id, Employee emp){
+	        EmployeeDAO dao = new EmployeeDAO();
+	        int count = dao.updateEmployee(id, emp);
+	        if(count==0){
+	            return Response.status(Response.Status.BAD_REQUEST).build();
+	        }
+	        return Response.ok().build();
+	    }
+	    
+	    @DELETE
+	    @Path("/delete/{id}")
+	    @Consumes("application/json")
+	    public Response deleteEmployee(@PathParam("id") int id){
+	        EmployeeDAO dao = new EmployeeDAO();
+	        int count = dao.deleteEmployee(id);
+	        if(count==0){
+	            return Response.status(Response.Status.BAD_REQUEST).build();
+	        }
+	        return Response.ok().build();
+	    }
 	}
- 
-}
