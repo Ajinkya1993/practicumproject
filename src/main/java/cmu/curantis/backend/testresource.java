69a6eb6bc99bf3d3f708b71be7bea8487f7e7ebb
Ajinkya1993 <animbalk@andrew.cmu.edu>
2017-07-23 13:09:21 -0400

    CircleSubsDAO tested and completed. Working fine

--- src/main/java/cmu/curantis/backend/testresource.java
+++ src/main/java/cmu/curantis/backend/testresource.java
@@ -15,56 +15,89 @@ import cmu.curantis.dao.SessionUtil;
 import cmu.curantis.entities.CaregiverInfoBean;
 import cmu.curantis.entities.CircleSubsBean;
 
+
 @Path("/test")
 public class testresource {
 	@POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-	public void test () {
+	@Path("/testdelete")
+	public void testdelete () {
 		
 		CircleSubsBean bean = new CircleSubsBean();
-		bean.setCardMemberFirstName("Ajinkya");
+		bean.setCardMemberFirstName("Shubham");
 		bean.setCardMemberLastName("Nimbalkar");
+		bean.setCircleId(3);
 		bean.setCircleName("mycircle");
 		CircleSubsDAO dao = new CircleSubsDAO();
 		Session session = SessionUtil.getSession();        
         Transaction tx = session.beginTransaction();
-        System.out.println(dao.addCircleSubs(session, bean));        
+        dao.deleteCircleSubs(session, bean);
+                
         tx.commit();
         session.close();
 		
 		
+	}
+	
+	@POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+	@Path("/testadd")
+	public void testadd () {
 		
-		/*
-		LoginOutput output = new LoginOutput();
-		
-		CaregiverInfoDAO caregiverdao = new CaregiverInfoDAO();
+		CircleSubsBean bean = new CircleSubsBean();
+		bean.setCardMemberFirstName("Shubham");
+		bean.setCardMemberLastName("Nimbalkar");
+		bean.setCircleId(1);
+		bean.setCircleName("mycircle");
+		CircleSubsDAO dao = new CircleSubsDAO();
 		Session session = SessionUtil.getSession();        
         Transaction tx = session.beginTransaction();
-        CaregiverInfoBean caregiver = caregiverdao.getCaregiverInfo(session, input.getEmail());
-        if (caregiver != null && caregiver.getRegisteredStatus()) {
-        	output.setMessage("Account already exist!");
-        	output.setSuccess(false);
-        } else {
-        	caregiver = new CaregiverInfoBean();
-        	caregiver.setEmail(input.getEmail());
-        	caregiver.setFirstName(input.getFirstName());
-        	caregiver.setMiddleName(input.getMiddleName());
-        	caregiver.setLastName(input.getLastName());
-        	caregiver.setPassword(input.getPassword());
-        	caregiver.setAddress(input.getAddress());
-        	caregiver.setPhoneNumber(input.getPhoneNo());
-        	caregiver.setRegisteredStatus(true);
-        	session.saveOrUpdate(caregiver);
-        	output.setFirstName(input.getFirstName());
-        	output.setLastName(input.getLastName());
-        	output.setMessage("Register success!");
-        	output.setSuccess(true);
-        }
+        System.out.println(dao.addCircleSubs(session, bean));                
         tx.commit();
         session.close();
 		
-		return output;
-		*/
+	}
+	
+	@POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+	@Path("/testget")
+	public void testget () {
+		
+		CircleSubsBean bean = new CircleSubsBean();
+		bean.setCardMemberFirstName("Shubham");
+		bean.setCardMemberLastName("Nimbalkar");
+		bean.setCircleId(2);
+		bean.setCircleName("mycircle");
+		CircleSubsDAO dao = new CircleSubsDAO();
+		Session session = SessionUtil.getSession();        
+        Transaction tx = session.beginTransaction();
+        System.out.println(dao.getCircleSubs(session, bean).getCardMemberFirstName());                
+        tx.commit();
+        session.close();
+		
+	}
+	
+	@POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+	@Path("/testupdate")
+	public void testupdate () {
+		
+		CircleSubsBean bean = new CircleSubsBean();
+		bean.setCardMemberFirstName("Ajinkya");
+		bean.setCardMemberLastName("Nimbalkar");
+		bean.setCircleId(3);
+		bean.setCircleName("mycircle");
+		CircleSubsDAO dao = new CircleSubsDAO();
+		Session session = SessionUtil.getSession();        
+        Transaction tx = session.beginTransaction();
+        System.out.println(dao.updateCircleSubs(session, bean));
+        
+        tx.commit();
+        session.close();
+		
 	}
 }
